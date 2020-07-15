14d44bdcbb07cc4855ed6b9732a82204684d6124
Radha Brito <rbritom@gmail.com>
2020-07-15 17:41:34 -0400

    removed unnecessary files

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,5 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-
-import Header from "./header"
-// import "./layout.css"
-import "./main.scss"
 
 const Layout = ({ children }) => {
   return (
