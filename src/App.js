b47819f0dd8d75144413e7a6bd08d1ed05156384
Radha Brito <rbritom@gmail.com>
2020-07-15 18:00:47 -0400

    more reductions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import Layout from "./components/layout"
 import Board from "./components/board.js"
 
 function App() {
   return (
-    <Layout>
+    <main>
       <Board />
-    </Layout>
+    </main>
   );
 }
 
